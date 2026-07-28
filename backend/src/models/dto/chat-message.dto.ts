327c4ca94e4f6ffcfc4d1b5d2e334d760c30157c
Esteban <esteban.piga.developer@gmail.com>
2026-07-28 00:12:58 -0300

    Validate max characters per chat message

--- backend/src/models/dto/chat-message.dto.ts
+++ backend/src/models/dto/chat-message.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Max, MaxLength } from 'class-validator';
 
 export class ChatMessageDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(50, { message: 'Message is too long. Maximum length is 50 characters.' })
   message!: string;
 }
