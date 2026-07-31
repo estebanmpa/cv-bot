4d61da89d3cf145bc1d218c84459bf926680de73
Esteban <esteban.piga.developer@gmail.com>
2026-07-31 15:16:28 -0300

    Add log to chat message

--- backend/src/controllers/chat.controller.ts
+++ backend/src/controllers/chat.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { ClaudeService } from '../services/claude.service';
 import { ChatMessageDto } from '../models/dto/chat-message.dto';
 
 @Controller('chat')
 export class ChatController {
-  constructor(private readonly claudeService: ClaudeService) {}
+  constructor(private readonly claudeService: ClaudeService) { }
 
+  private readonly logger = new Logger(ChatController.name);
+  
   @Post()
   async create(@Body() body: ChatMessageDto) {
+    this.logger.log(`receiveChatMessage: ${JSON.stringify(body)}`);
     const reply = await this.claudeService.replyToMessage(body.message, body.chatId);
     return { reply };
   }
